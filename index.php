f1e90a39bbf152d83d3eb9d4fbe11ecd6132357f
ravasingh <ravasingh@algomau.ca>
2024-05-17 18:28:24 +0000

    Added Welcome, Today Date and Logout link in Index page

--- index.php
+++ index.php
@@ -1,16 +1,29 @@
 <?php
-    session_start();
-    if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
-        header("Location: login.php");
-        exit;
-    }
-?> 
+session_start();
+if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+    header("Location: login.php");
+    exit;
+}
+?>
 <!DOCTYPE html>
-<html>
+<html lang="en">
     <head>
         <title>Ravanpreet Singh</title>
     </head>
+    
     <body>
-        <h1>Assignment.1</h1>
+        <header>
+            <h1>Assignment-1</h1>
+        </header>
+        
+        <main>
+             <?php echo "Welcome, " . $_SESSION['username'] . "!<br>"; ?>
+             <?php echo "Today's date is " . date("l, F jS Y") . "."; ?>
+        </main>
+       
+        <footer>
+            <p><a href="logout.php">Click here to Logout</a></p>
+         </footer>
+        
     </body>
 </html>
