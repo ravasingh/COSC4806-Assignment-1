e40e3cff065170488963de5d8dc3ffa6ead59923
ravasingh <ravasingh@algomau.ca>
2024-05-17 16:49:31 +0000

    Successfully logged in with hardcoded username and password

--- validate.php
+++ validate.php
@@ -3,4 +3,16 @@
 $code_username = "ravan";
 $code_password = "123";
 
+$correct_username = $_REQUEST["username"];
+$correct_password = $_REQUEST["password"];
+
+if($code_username == $correct_username && $code_password == $correct_password)
+  {
+  echo "successfully logged in!";
+  }
+else
+{
+  echo "failed to login. Invalid username or password";
+}
+
 ?>
