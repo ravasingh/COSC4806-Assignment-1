74d34a45cbbd227b4634ea098d90a07d835e100b
ravasingh <ravasingh@algomau.ca>
2024-05-17 17:28:47 +0000

    Added code to set session variables upon successful login and redirect the user to index.php

--- validate.php
+++ validate.php
@@ -1,17 +1,31 @@
 <?php
-
+session_start();
 $code_username = "ravan";
 $code_password = "123";
 
-$correct_username = $_REQUEST["username"];
-$correct_password = $_REQUEST["password"];
+$username = $_REQUEST["username"];
+$password = $_REQUEST["password"];
 
-if($code_username == $correct_username && $code_password == $correct_password)
+if($code_username == $username && $code_password == $password)
   {
+    $_SESSION['authenticated'] = true;
+    $_SESSION['username'] = $username;
+    header("Location: index.php");
+    exit;
   echo "successfully logged in!";
   }
 else
 {
+  if (isset($_SESSION['login_attempts'])) 
+  {
+      $_SESSION['login_attempts']++;
+  } 
+  else 
+  {
+      $_SESSION['login_attempts'] = 1;
+  }
+  header("Location: login.php");
+  exit;
   echo "failed to login. Invalid username or password";
 }
 
