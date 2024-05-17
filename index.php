058ebfc6f7409a2e377711bf0bf2c7d2be8c424f
ravasingh <ravasingh@algomau.ca>
2024-05-17 16:19:03 +0000

    Added the Submit button to the login form

--- index.php
+++ index.php
@@ -12,6 +12,8 @@
             <label for="password">Password:</label>
             <input type="password" id="password" name="password" required>
             <br><br>
+          <input type="submit" value="Login">
+          
         </form>
 
     </body>
