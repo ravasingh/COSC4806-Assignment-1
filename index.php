ea8a39b494538e5fb4a0bc210210b612a82399aa
ravasingh <ravasingh@algomau.ca>
2024-05-17 18:20:55 +0000

    Modify the index.php page, to redirect the user to the login page if authentication is not true.

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
+<?php
+    session_start();
+    if (!isset($_SESSION['authenticated']) || $_SESSION['authenticated'] !== true) {
+        header("Location: login.php");
+        exit;
+    }
+?> 
 <!DOCTYPE html>
 <html>
     <head>
@@ -5,6 +12,5 @@
     </head>
     <body>
         <h1>Assignment.1</h1>
-
     </body>
 </html>
